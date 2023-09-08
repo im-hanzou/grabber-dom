762ec1427d03112b0726191351099798c276aa68
zaen hxr <83585135+zaenhaxor@users.noreply.github.com>
2023-09-09 00:42:58 +0700

    Update per_page.php

--- per_page.php
+++ per_page.php
@@ -1,5 +1,4 @@
 <?php
-
 $hijau = "\33[32m";
 $kuning = "\033[33m";
 $merah = "\033[31m";
@@ -11,22 +10,16 @@ $banner = "
             Coded by Zaen
          about.me/zaenhxr
 {$cyan}******************************************{$reset}";
-
 echo "$banner\n";
-
 function scraping($from_page, $to_page) {
     global $hijau, $cyan, $merah, $kuning, $reset;
     $out_file = fopen('page-domain.txt', 'w');
-    
     for ($page = $from_page; $page <= $to_page; $page++) {
         $url = "https://www.dubdomain.com/index.php?page={$page}";
         $req = file_get_contents($url);
-        
         preg_match_all('/<a href="q\.php\?domain=([^"]+)" title="[^"]+">[^<]+<\/a>/', $req, $regex);
         $total_domains = count($regex[1]);
-        
         echo "[{$merah}+{$reset}] Page {$hijau}{$page}{$reset} {$kuning}=>{$reset} Total: {$hijau}{$total_domains}{$reset}\n";
-        
         foreach ($regex[1] as $domain) {
             fwrite($out_file, $domain . "\n");
         }
